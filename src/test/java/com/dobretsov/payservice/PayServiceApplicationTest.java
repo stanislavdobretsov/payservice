af5e2fc9a2c181ecc3abdabddee28a6520c3c927
Stanislav Dobretsov <thestasoundor@gmail.com>
2019-07-25 19:00:10 +0700

    empty spring boot application commit

---
+++ src/test/java/com/dobretsov/payservice/PayServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.dobretsov.payservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class PayServiceApplicationTest {
+
+    @Test
+    public void contextTest() {
+
+    }
+
+}
